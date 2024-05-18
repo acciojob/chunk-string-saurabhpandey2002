b376bda29cc9af18db4dd990de7d361a03715d32
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-18 14:46:45 +0530

    add

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+let ans = [];
+
 function stringChop(str, size) {
-  // your code here
+  if(str==='' || str===null){
+	return [];
+  }
+  for (let i = 0; i < str.length; i += size) {
+    slice(str, i, i + size);
+  }
+
+  return ans;
 }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+function slice(str, start, end) {
+  let sliced = str.slice(start, end);
+  ans.push(sliced);
+}
